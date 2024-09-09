d4799818e8f5839ce42dd7ecfaf20c8b385d9d06
Hoyoung Park <qkrghdud369@gmail.com>
2024-09-09 23:11:12 +0900

    first commit

---
+++ src/main/java/project/BoardProject/controller/BoardController.java
@@ -0,0 +1,14 @@
+package project.BoardProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+    @GetMapping("Board")
+    public String Board(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "Board";
+    }
+}
